cb9fb3662642e14742734073911362c7fce3c9bc
Kirill Kumchenko <kirillkumchenko@gmail.com>
2024-03-24 19:57:08 +0200

    init

---
+++ src/main/java/com/example/comfywishlist/ComfyWishListApplication.java
@@ -0,0 +1,13 @@
+package com.example.comfywishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComfyWishListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComfyWishListApplication.class, args);
+    }
+
+}
